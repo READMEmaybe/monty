2558b8b65359bc85be5535b47f23736720c82cc6
READMEmaybe <haroune11411@gmail.com>
2023-08-27 17:22:42 +0100

    fix task 4

--- funcs1.c
+++ funcs1.c
@@ -1,15 +1,25 @@
 #include "monty.h"
 
 /**
- * add - Adds the top two values of a stack_t linked list.
- * @stack: A pointer to the top mode node of a stack_t linked list.
- * @line_number: The current working line number of a Monty bytecodes file.
- *
- * Description: The result is stored in the second value node
- *              from the top and the top value  is removed.
+ * add - Adds the top two elements of the stack.
+ * @stack: A pointer to the pointer to the stack.
+ * @line_number: The current line number in the Monty script.
  */
 void add(stack_t **stack, unsigned int line_number)
 {
+	int len = 0;
+	stack_t *tmp;
+
+	tmp = (*stack)->next;
+	for (; tmp != NULL; tmp = tmp->next, len++)
+		;
+	if (len < 2)
+	{
+		fprintf(stderr, "L%u: can't add, stack too short\n", line_number);
+		free_stack(stack);
+		free_monty();
+		exit(EXIT_FAILURE);
+	}
 	(*stack)->next->next->n += (*stack)->next->n;
 	pop(stack, line_number);
 }
