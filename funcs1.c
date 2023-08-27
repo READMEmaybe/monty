ba85cbcd8c1455c0d7d922b904031cb8f3d390cc
READMEmaybe <haroune11411@gmail.com>
2023-08-27 17:40:48 +0100

    fix task 6

--- funcs1.c
+++ funcs1.c
@@ -25,22 +25,42 @@ void add(stack_t **stack, unsigned int line_number)
 }
 
 /**
- * sub - Subtracts the second value from the top of
- *             a stack_t linked list by the top value.
- * @stack: A pointer to the top mode node of a stack_t linked list.
- * @line_number: The current working line number of a Monty bytecodes file.
- *
- * Description: The result is stored in the second value node
- *              from the top and the top value is removed.
+ * nop - As useful as I am
+ * @stack: A pointer to the pointer to the stack.
+ * @line_number: The current line number in the Monty script.
+ */
+void nop(stack_t **stack, unsigned int line_number)
+{
+	(void)stack;
+	(void)line_number;
+}
+
+/**
+ * sub - Subtracts the top element from the second element of the stack.
+ * @stack: A pointer to the pointer to the stack.
+ * @line_number: The current line number in the Monty script.
  */
 void sub(stack_t **stack, unsigned int line_number)
 {
+	int len = 0;
+	stack_t *tmp;
+
+	tmp = (*stack)->next;
+	for (; tmp != NULL; tmp = tmp->next, len++)
+		;
+	if (len < 2)
+	{
+		fprintf(stderr, "L%u: can't sub, stack too short\n", line_number);
+		free_stack(stack);
+		free_monty();
+		exit(EXIT_FAILURE);
+	}
 	(*stack)->next->next->n -= (*stack)->next->n;
 	pop(stack, line_number);
 }
 
 /**
- * div - Divides the second value from the top of
+ * div_ - Divides the second value from the top of
  *             a stack_t linked list by the top value.
  * @stack: A pointer to the top mode node of a stack_t linked list.
  * @line_number: The current working line number of a Monty bytecodes file.
--- funcs2.c
+++ funcs2.c
@@ -4,16 +4,7 @@ void nop(stack_t **stack, unsigned int line_number);
 void pchar(stack_t **stack, unsigned int line_number);
 void pstr(stack_t **stack, unsigned int line_number);
 
-/**
- * nop - Does absolutely nothing for the Monty opcode 'nop'.
- * @stack: A pointer to the top mode node of a stack_t linked list.
- * @line_number: The current working line number of a Monty bytecodes file.
- */
-void nop(stack_t **stack, unsigned int line_number)
-{
-	(void)stack;
-	(void)line_number;
-}
+
 
 /**
  * pchar - Prints the character in the top value
